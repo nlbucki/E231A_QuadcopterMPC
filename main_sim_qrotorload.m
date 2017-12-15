ec5a84c91b8550d870f9fb0f74948b7776506e12
David M <32400304+davidm3i@users.noreply.github.com>
2017-12-14 18:38:32 -0800

    include traj in workspace mat files
    workspace mat files for test cases should now be complete.

--- main_sim_qrotorload.m
+++ main_sim_qrotorload.m
@@ -18,51 +18,51 @@ sys = Quadrotorload(params);
 sys.controller = @controller;
 
 %% Testcase 1 - Keyhole
-% % Don't redefine the problem without specifying a new name!!!
-% TCfolder = 'Testcase_1_keyhole_2m';
-% % start and goal states and state constraints
-% x0 = [-12;0;0;0;0;0;0;0];
-% xF = [0;0;0;0;0;0;0;0];
-% xU = [2; 2; pi/2; pi/2; 5; 5; 10; 10];
-% xL = [-13; -13; -xU(3:end)];
-% % number of time steps
-% N=80;
-% % obstacles
-% O ={Polyhedron('V',[-6 -5.5; -6 xU(2)+0.1; -7 -5.5; -7 xU(2)+0.1]),...
-%     Polyhedron('V',[-6 -7.5; -6 xL(2)-0.1; -7 -7.5; -7 xL(2)-0.1])};
-% % initial guess
-% xinit = zeros(length(xF),N+1);
-% xinit(1,1:N/2-2) = linspace(x0(1),-7,N/2-2);
-% xinit(1,N/2-1:N/2+1) = linspace(-7,-6,3);
-% xinit(1,N/2+2:end) = linspace(-6,xF(1),N/2);
-% xinit(2,1:N/2-2) = linspace(x0(2),-6.5,N/2-2);
-% xinit(2,N/2-1:N/2+1) = linspace(-6.5,-6.5,3);
-% xinit(2,N/2+2:end) = linspace(-6.5,xF(2),N/2);
-% % trajectory generation
-% traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
-% % Save Testcase specification and traj in .mat file
-% save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
-%     'sys','x0','xF','xinit','xL','xU')
+% Don't redefine the problem without specifying a new name!!!
+TCfolder = 'Testcase_1_keyhole_2m';
+% start and goal states and state constraints
+x0 = [-12;0;0;0;0;0;0;0];
+xF = [0;0;0;0;0;0;0;0];
+xU = [2; 2; pi/2; pi/2; 5; 5; 10; 10];
+xL = [-13; -13; -xU(3:end)];
+% number of time steps
+N=80;
+% obstacles
+O ={Polyhedron('V',[-6 -5.5; -6 xU(2)+0.1; -7 -5.5; -7 xU(2)+0.1]),...
+    Polyhedron('V',[-6 -7.5; -6 xL(2)-0.1; -7 -7.5; -7 xL(2)-0.1])};
+% initial guess
+xinit = zeros(length(xF),N+1);
+xinit(1,1:N/2-2) = linspace(x0(1),-7,N/2-2);
+xinit(1,N/2-1:N/2+1) = linspace(-7,-6,3);
+xinit(1,N/2+2:end) = linspace(-6,xF(1),N/2);
+xinit(2,1:N/2-2) = linspace(x0(2),-6.5,N/2-2);
+xinit(2,N/2-1:N/2+1) = linspace(-6.5,-6.5,3);
+xinit(2,N/2+2:end) = linspace(-6.5,xF(2),N/2);
+% trajectory generation
+traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
+% Save Testcase specification and traj in .mat file
+save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
+    'sys','x0','xF','xinit','xL','xU')
 
 %% Testcase 2 - Inverted Pendulum
-% % Don't redefine the problem without specifying a new name!!!
-% TCfolder = 'Testcase_2_inverted_pendulum';
-% % start and goal states and state constraints
-% x0 = [0;0;0;0;0;0;0;0];
-% xF = [0;0;pi;0;0;0;0;0];
-% xU = [2; 2; pi; pi; 5; 5; 10; 10];
-% xL = [-2; -2; -xU(3:end)];
-% % number of time steps
-% N=80;
-% % obstacles
-% O ={};
-% % initial guess
-% xinit = zeros(length(xF),N+1);
-% % trajectory generation
-% traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
-% % Save Testcase specification and traj in .mat file
-% save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
-%     'sys','x0','xF','xinit','xL','xU')
+% Don't redefine the problem without specifying a new name!!!
+TCfolder = 'Testcase_2_inverted_pendulum';
+% start and goal states and state constraints
+x0 = [0;0;0;0;0;0;0;0];
+xF = [0;0;pi;0;0;0;0;0];
+xU = [2; 2; pi; pi; 5; 5; 10; 10];
+xL = [-2; -2; -xU(3:end)];
+% number of time steps
+N=80;
+% obstacles
+O ={};
+% initial guess
+xinit = zeros(length(xF),N+1);
+% trajectory generation
+traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
+% Save Testcase specification and traj in .mat file
+save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
+    'sys','x0','xF','xinit','xL','xU')
 
 %% Testcase 3 - Triangles
 % Don't redefine the problem without specifying a new name!!!
@@ -89,7 +89,7 @@ xinit(2,2*floor(N/3)+1:end) = linspace(-7.5,xF(2),N+1-2*floor(N/3));
 traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
 % Save Testcase specification and traj in .mat file
 save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
-    'sys','x0','xF','xinit','xL','xU')
+    'sys','traj','x0','xF','xinit','xL','xU')
 
 %% Generate trajectory to track
 % load trajectory
