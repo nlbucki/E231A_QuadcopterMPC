c06693e1b0e76fdd37a7724c9dfa8b6aaf78bee4
David M <32400304+davidm3i@users.noreply.github.com>
2017-12-14 22:00:55 -0800

    Change to Euler discretization

--- @Quadrotorload/discretizeLinearizeQuadrotorload.m
+++ @Quadrotorload/discretizeLinearizeQuadrotorload.m
@@ -5,20 +5,20 @@ function [Ad, Bd] = discretizeLinearizeQuadrotorload(obj, Ts, xk, uk)
 
 [A, B] = obj.linearizeQuadrotor(xk, uk);
 
-% Ad = eye(obj.nDof) + Ts*A;
-% Bd = Ts*B;
-% Discretize the matrices
-if rank(A) - min(size(A)) == 0
-    % Standard equations (Bd not via integral but special equation for
-    % non-singular A matrix)
-    Ad = expm(A*Ts);
-    Bd = A\(Ad-eye(size(Ad,1)))*B;
-else
-    % Computation via one big exponential matrix
-    exponMat = expm([A, B; zeros(size(B,2),size(A,2)+size(B,2))]*Ts);
-    Ad = exponMat(1:size(A,1),1:size(A,2));
-    Bd = exponMat(1:size(B,1),(size(A,2)+1):(size(A,2)+size(B,2)));
-end
+Ad = eye(obj.nDof) + Ts*A;
+Bd = Ts*B;
+% % Discretize the matrices
+% if rank(A) - min(size(A)) == 0
+%     % Standard equations (Bd not via integral but special equation for
+%     % non-singular A matrix)
+%     Ad = expm(A*Ts);
+%     Bd = A\(Ad-eye(size(Ad,1)))*B;
+% else
+%     % Computation via one big exponential matrix
+%     exponMat = expm([A, B; zeros(size(B,2),size(A,2)+size(B,2))]*Ts);
+%     Ad = exponMat(1:size(A,1),1:size(A,2));
+%     Bd = exponMat(1:size(B,1),(size(A,2)+1):(size(A,2)+size(B,2)));
+% end
 
 end
 
