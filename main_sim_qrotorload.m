b7ae4d552c747019b62b8d52653c0517dc81cc29
David M <32400304+davidm3i@users.noreply.github.com>
2017-12-14 20:36:04 -0800

    TC2 N=60 and changed x0 and xF
    For the sake of a nicer plot

--- main_sim_qrotorload.m
+++ main_sim_qrotorload.m
@@ -45,51 +45,51 @@ sys.controller = @controller;
 %     'sys','traj','x0','xF','xinit','xL','xU')
 
 %% Testcase 2 - Inverted Pendulum
-% % Don't redefine the problem without specifying a new name!!!
-% TCfolder = 'Testcase_2_inverted_pendulum';
-% % start and goal states and state constraints
-% x0 = [0;0;0;0;0;0;0;0];
-% xF = [0;0;pi;0;0;0;0;0];
-% xU = [2; 2; pi; pi; 5; 5; 10; 10];
-% xL = [-2; -2; -xU(3:end)];
-% % number of time steps
-% N=80;
-% % obstacles
-% O ={};
-% % initial guess
-% xinit = zeros(length(xF),N+1);
-% % trajectory generation
-% traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
-% % Save Testcase specification and traj in .mat file
-% save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
-%     'sys','traj','x0','xF','xinit','xL','xU')
-
-%% Testcase 3 - Triangles
 % Don't redefine the problem without specifying a new name!!!
-TCfolder = 'Testcase_3_triangles';
+TCfolder = 'Testcase_2_inverted_pendulum';
 % start and goal states and state constraints
-x0 = [-11;-11;0;0;0;0;0;0];
-xF = [0;0;0;0;0;0;0;0];
-xU = [2; 2; pi/2; pi/2; 5; 5; 10; 10];
-xL = [-13; -13; -xU(3:end)];
+x0 = [-1.5;0;0;0;0;0;0;0];
+xF = [1.5;0;pi;0;0;0;0;0];
+xU = [2; 2; pi; pi; 5; 5; 10; 10];
+xL = [-2; -2; -xU(3:end)];
 % number of time steps
-N=120;
+N=50;
 % obstacles
-O ={Polyhedron('V',[-2 xU(2)+0.1; -3.5 -6.5; -5 xU(2)+0.1]),...
-    Polyhedron('V',[-4 xL(2)-0.1; -7 -3.5; -10 xL(2)-0.1])};
+O ={};
 % initial guess
 xinit = zeros(length(xF),N+1);
-xinit(1,1:floor(N/3)) = linspace(x0(1),-7,floor(N/3));
-xinit(1,floor(N/3)+1:2*floor(N/3)) = linspace(-7,-3.5,floor(N/3));
-xinit(1,2*floor(N/3)+1:end) = linspace(-3.5,xF(1),N+1-2*floor(N/3));
-xinit(2,1:floor(N/3)) = linspace(x0(2),-2.5,floor(N/3));
-xinit(2,floor(N/3)+1:2*floor(N/3)) = linspace(-2.5,-7.5,floor(N/3));
-xinit(2,2*floor(N/3)+1:end) = linspace(-7.5,xF(2),N+1-2*floor(N/3));
 % trajectory generation
 traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
 % Save Testcase specification and traj in .mat file
 save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
     'sys','traj','x0','xF','xinit','xL','xU')
+
+%% Testcase 3 - Triangles
+% % Don't redefine the problem without specifying a new name!!!
+% TCfolder = 'Testcase_3_triangles';
+% % start and goal states and state constraints
+% x0 = [-11;-11;0;0;0;0;0;0];
+% xF = [0;0;0;0;0;0;0;0];
+% xU = [2; 2; pi/2; pi/2; 5; 5; 10; 10];
+% xL = [-13; -13; -xU(3:end)];
+% % number of time steps
+% N=120;
+% % obstacles
+% O ={Polyhedron('V',[-2 xU(2)+0.1; -3.5 -6.5; -5 xU(2)+0.1]),...
+%     Polyhedron('V',[-4 xL(2)-0.1; -7 -3.5; -10 xL(2)-0.1])};
+% % initial guess
+% xinit = zeros(length(xF),N+1);
+% xinit(1,1:floor(N/3)) = linspace(x0(1),-7,floor(N/3));
+% xinit(1,floor(N/3)+1:2*floor(N/3)) = linspace(-7,-3.5,floor(N/3));
+% xinit(1,2*floor(N/3)+1:end) = linspace(-3.5,xF(1),N+1-2*floor(N/3));
+% xinit(2,1:floor(N/3)) = linspace(x0(2),-2.5,floor(N/3));
+% xinit(2,floor(N/3)+1:2*floor(N/3)) = linspace(-2.5,-7.5,floor(N/3));
+% xinit(2,2*floor(N/3)+1:end) = linspace(-7.5,xF(2),N+1-2*floor(N/3));
+% % trajectory generation
+% traj = traj_gen_QRL_polyhedron(sys,O,x0,xF,xU,xL,xinit,N,TCfolder);
+% % Save Testcase specification and traj in .mat file
+% save(['results\' TCfolder '\workspace.mat'],'N','O','params',...
+%     'sys','traj','x0','xF','xinit','xL','xU')
 
 %% Generate trajectory to track
 % load trajectory
